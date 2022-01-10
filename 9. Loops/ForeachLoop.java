20f2e2bd99d9850f6005df2c3179340cebf17a08
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2022-01-10 11:51:39 +0600

    Create ForeachLoop.java

---
+++ 9. Loops/ForeachLoop.java
@@ -0,0 +1,18 @@
+public class ForeachLoop {
+	
+	public static void main(String[] args) {
+		System.out.println("=========== foreach loop");
+		int[] arr = {1, 2, 3};
+		int sum = 0;
+		for (int number : arr) {
+			sum += number;
+			System.out.println("Number: " + number);
+		}
+		System.out.println("Sum: " + sum);
+
+		for (int i : arr) {
+			
+		}
+	}
+
+}
