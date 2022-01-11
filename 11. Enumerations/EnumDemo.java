2c25740bd8468eca9bc886f0a7c0012b3014437e
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2022-01-11 10:08:41 +0600

    Add files via upload

---
+++ 11. Enumerations/EnumDemo.java
@@ -0,0 +1,59 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EnumDemo {
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Priority priority = Priority.HIGH;
+
+        switch (priority) {
+            case HIGH:
+                System.out.println("High priority");
+                break;
+            case MEDIUM:
+                System.out.println("Medium priority");
+                break;
+            case LOW:
+                System.out.println("Low priority");
+                break;
+        }
+
+        System.out.println("============ Enum valueOf()");
+
+        Priority priority2 = Priority.valueOf("HIGH");
+        System.out.println(priority2);
+
+//		priority2 = Priority.valueOf("high"); // java.lang.IllegalArgumentException: No enum constant com.itbulls.learnit.javacore.enumerations.Priority.high
+
+
+        System.out.println("============ Enum comparison");
+
+        System.out.println("Priority.HIGH == Priority.MEDIUM: "
+                + (priority == Priority.MEDIUM));	// false
+
+        System.out.println("Priority.HIGH == Priority.HIGH: "
+                + (priority == Priority.HIGH));	// true
+
+
+        System.out.println("============ Enum ordinal()");
+// ordinal is like array index
+        System.out.println("Priority.HIGH.ordinal(): " + Priority.HIGH.ordinal());
+        System.out.println("Priority.MEDIUM.ordinal(): " + Priority.MEDIUM.ordinal());
+
+
+        System.out.println("============ Enum iteration");
+
+        Priority[] values = Priority.values();
+        for (Priority priority3 : values) {
+            System.out.println(priority3);
+        }
+
+
+        System.out.println("============ Enum fields and methods");
+
+        System.out.println("Month.JANUARY.getDaysAmount(): " + Month.JANUARY.getDaysAmount());
+    }
+}
---
+++ 11. Enumerations/Month.java
@@ -0,0 +1,16 @@
+public enum Month {
+
+    JANUARY(31), FEBRUARY(28), MARCH(31), APRIL(30), MAY(31), JUNE(30), JULY(31),
+    AUGUST(31), SEPTEMBER(30), OCTOBER(31), NOVEMBER(30), DECEMBER(31);
+
+    private int daysAmount;
+
+    private Month(int daysAmount) {
+        this.daysAmount = daysAmount;
+    }
+
+    public int getDaysAmount() {
+        return this.daysAmount;
+    }
+
+}
