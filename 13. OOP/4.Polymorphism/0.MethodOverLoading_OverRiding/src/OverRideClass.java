55c817d5ae4b0264d45ce29d06f883fc2e8df78f
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2022-09-23 22:20:58 +0600

    Update OverRideClass.java

--- 13. OOP/4.Polymorphism/0.MethodOverLoading_OverRiding/src/OverRideClass.java
+++ 13. OOP/4.Polymorphism/0.MethodOverLoading_OverRiding/src/OverRideClass.java
@@ -1,6 +1,6 @@
 // Parent Class
-// If it's extended to child class and child class have display method. Only parent classes method will execute if child class method
-// doesn't use @override annotation before the method
+// If it's extended to child class and child class have display method. Only child classes method will execute if child class method
+// has the same name. Using @Override or not doesn't matter
 public class OverRideClass { 
 
     public void display(){
