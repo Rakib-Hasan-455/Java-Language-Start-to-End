64857f972c55dde09aaba2235a9d59739d06c77e
Md. Rakibul Hasan <hasanrakib455@gmail.com>
2024-01-21 12:03:46 +0600

    Update OverRideClass.java

--- 13. OOP/4.Polymorphism/0.MethodOverLoading_OverRiding/src/OverRideClass.java
+++ 13. OOP/4.Polymorphism/0.MethodOverLoading_OverRiding/src/OverRideClass.java
@@ -1,7 +1,7 @@
 // Parent Class
 // If it's extended to child class and child class have display method. Only child classes method will execute if child class method
 // has the same name. Using @Override or not doesn't matter
-public class OverRideClass { 
+public class OverRideClass { // OverRideClass is the main class (parent)
 
     public void display(){
         System.out.println("Override Class with zero parameter");
